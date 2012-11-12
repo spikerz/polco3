8917cfcd541a097e5f7f57c850fe21ebe74f18cd
tim booher <tim@theboohers.org>
2012-11-12 06:52:07 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polco3::Application.config.secret_token = '2d6dbdd6cc9e2d30e91b658f07520e4e8497d9f7e4998a0f8bd0112052753267bc5825ebd2e0cf76621d5f935a22a839021c6616732f4bb58fd003c71bea0822'
