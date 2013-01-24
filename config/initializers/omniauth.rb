147426b6fc6ffc23fab999f3b49486726f89090b
Tim Booher <tim@theboohers.org>
2013-01-23 22:16:12 -0500

    add omniauth and new relationships

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   # https://dev.twitter.com/apps/3670983/oauth
   provider :identity, on_failed_registration: lambda { |env|
     IdentitiesController.action(:new).call(env)
